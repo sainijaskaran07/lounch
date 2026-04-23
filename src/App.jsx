e4cdcf3b2eca8621a5d67ccd611d795883f8c4fa
Jaskaran Singh Saini <sainijaskaransingh429@gmail.com>
2026-04-23 21:46:58 +0530

    test without css

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
 export default function App() {
   return (
-    <div className="coming-page">
-      <div className="coming-card">
-        <div className="grain"></div>
-
-        <div className="star star-top">
-          <span className="v"></span>
-          <span className="h"></span>
-        </div>
-
-        <div className="star star-bottom">
-          <span className="v"></span>
-          <span className="h"></span>
-        </div>
-
-        <div className="star star-small">
-          <span className="v"></span>
-          <span className="h"></span>
-        </div>
-
-        <div className="content">
-          <h1>
-            <span>Launching</span>
-            <span>Soon</span>
-          </h1>
-          <p>Something beautiful is on the way</p>
-        </div>
-      </div>
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "red",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "48px",
+        fontWeight: "700",
+      }}
+    >
+      CHITHIYAN IS LIVE
     </div>
   );
 }
