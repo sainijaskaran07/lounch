7a9a2bc9bb87fcaea08225ab0d988a9810959d41
Jaskaran Singh Saini <sainijaskaransingh429@gmail.com>
2026-04-23 21:40:55 +0530

    add launching soon UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 export default function App() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        background: "red",
-        color: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "40px",
-        fontWeight: "bold",
-      }}
-    >
-      APP IS WORKING
+    <div className="coming-page">
+      <div className="coming-card">
+        <div className="grain"></div>
+
+        {/* Stars */}
+        <div className="star star-top">
+          <span className="v"></span>
+          <span className="h"></span>
+        </div>
+
+        <div className="star star-bottom">
+          <span className="v"></span>
+          <span className="h"></span>
+        </div>
+
+        <div className="star star-small">
+          <span className="v"></span>
+          <span className="h"></span>
+        </div>
+
+        {/* Content */}
+        <div className="content">
+          <h1>
+            <span>Launching</span>
+            <span>Soon</span>
+          </h1>
+          <p>Something beautiful is on the way</p>
+        </div>
+      </div>
     </div>
   );
 }
