8c41009849d922923904c41377359cdb90daccbf
Jaskaran Singh Saini <sainijaskaransingh429@gmail.com>
2026-04-25 14:53:21 +0530

    screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import soonBackground from "./assest/pic/soon.png";
 export default function App() {
   return (
     <main className="min-h-screen bg-[#eee8dd]">
-      <section className="relative flex min-h-screen items-center justify-center overflow-hidden px-4 sm:px-6">
+      <section className="relative flex min-h-[100svh] items-center justify-center overflow-hidden px-4 sm:px-6">
 
         {/* Background */}
         <div
@@ -15,8 +15,8 @@ export default function App() {
         />
 
         {/* Bottom social + notify */}
-        <div className="absolute bottom-10 z-10 flex w-full justify-center sm:bottom-16">
-          <div className="flex w-full max-w-md flex-col items-center gap-3">
+        <div className="absolute inset-x-0 bottom-8 z-10 flex justify-center px-4 sm:bottom-12 sm:px-6">
+          <div className="flex w-full max-w-sm flex-col items-center gap-3 sm:max-w-md">
             <a
               href="https://www.instagram.com/chithiyan.in?igsh=MTNiaHAwZG1la3Bjbw=="
               target="_blank"
