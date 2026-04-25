6e0f88a6bf690c8e9cef689752f6f4317d766209
Jaskaran Singh Saini <sainijaskaransingh429@gmail.com>
2026-04-25 13:15:32 +0530

    add launching soon design

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 export default function App() {
-  return (
-    <div className="coming-page">
-      <div className="coming-card">
-        <div className="grain"></div>
-
-        <div className="star star-top">
-          <span className="v"></span>
-          <span className="h"></span>
-        </div>
-
-        <div className="star star-bottom">
-          <span className="v"></span>
-          <span className="h"></span>
-        </div>
-
-        <div className="star star-small">
-          <span className="v"></span>
-          <span className="h"></span>
-        </div>
-
-        <div className="content">
-          <h1>
-            <span>Launching</span>
-            <span>Soon</span>
-          </h1>
-          <p>Something beautiful is on the way</p>
-        </div>
-      </div>
-    </div>
+  return(
+    <main className="min-h-screen bg-red-600 text-white flex items-center justify-center text-6xl font-bold">
+      TEST WORKING
+    </main>
   );
 }
